f845e66518f2ba374d41ec1a4ac2f03b64896243
infoclahus-dev <info.clah.us@gmail.com>
2025-12-15 17:59:21 +0700

    Updated AboutPage.tsx

--- components/AboutPage.tsx
+++ components/AboutPage.tsx
@@ -202,9 +202,9 @@ const AboutPage: React.FC = () => {
                {/* Headquarters */}
                <div className="group relative overflow-hidden rounded-3xl shadow-lg h-[400px]">
                   <div className="absolute inset-0 z-0">
-                     <img 
-                       src="https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?q=80&w=2070&auto=format&fit=crop" 
-                       alt="San Jose HQ" 
+                     <img
+                       src="https://i.postimg.cc/BZhtKbSG/1704446939_quan_binh_thanh_toa_nha_tnl_cho_thue_van_phong_hcm_jpg.jpg"
+                       alt="San Jose HQ"
                        className="w-full h-full object-cover transform group-hover:scale-110 transition-transform duration-700"
                      />
                      <div className="absolute inset-0 bg-gradient-to-t from-slate-900 via-slate-900/40 to-transparent" />
