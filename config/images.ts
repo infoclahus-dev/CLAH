6e2cdb3ee2de0fab06dffbb4c6874fe6f2653ec9
infoclahus-dev <info.clah.us@gmail.com>
2025-12-15 12:20:14 +0700

    Updated images.ts

--- config/images.ts
+++ config/images.ts
@@ -33,7 +33,7 @@ export const IMAGES = {
   ncaDesigns: {
     main: '/ncadesigns.jpg',
     gallery: [
-      'https://i.postimg.cc/bw0rZWXc/ncadesigns3.png',
+      'ncadesigns1.jpg',
       '/ncadesigns2.png',
       'https://i.postimg.cc/z86gcY0V/ncadesigns4.jpg',
       'https://i.postimg.cc/MqNQFJdM/ncadesigns7.jpg',
